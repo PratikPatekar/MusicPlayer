584ec89bec50e6bd946e35e3775a5b2480e9ceca
PratikPatekar <pratik.patekar3@gmail.com>
2018-07-20 11:20:05 +0530

    Update js.js

--- js.js
+++ js.js
@@ -1,11 +1,11 @@
 var volbar = document.getElementById("volume");
-		var output = document.getElementById("out");
-		output.innerHTML = volbar.value; // Display the default slider value
+var output = document.getElementById("out");
+output.innerHTML = volbar.value; // Display the default slider value
 
-		// Update the current slider value (each time you drag the slider handle)
-		volbar.oninput = function() {
-    		output.innerHTML = this.value;
-		}
+// Update the current slider value (each time you drag the slider handle)
+volbar.oninput = function() {
+	output.innerHTML = this.value;
+}
 
 
 
