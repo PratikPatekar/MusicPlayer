d34a8365da1965b1780715e8cbde43e754bde8bc
PratikPatekar <pratik.patekar3@gmail.com>
2018-07-27 17:09:33 +0530

    Update js.js

--- js.js
+++ js.js
@@ -33,7 +33,7 @@ var initiate = {
 	
 	playlist : function(songslist){
 		for(var i=0;i<songslist.length;++i){
-			plist.innerHTML += '<div id='+i+' onclick='changeSong(this.id)' class="songarray"><span>'+data.songs[i]+'</span></div>';
+			plist.innerHTML += '<div id='+i+' onclick='changeSong(this)' class="songarray"><span>'+data.songs[i]+'</span></div>';
 		}	
 	},
 	
